8008cc37ad63c7c7a88c3c58f642bf889d3408d5
TejasDesai007 <tejasdesai056@gmail.com>
2025-07-16 17:28:33 +0530

    again added some changes to blogScraper.js

--- Backend/routes/blogScraper.js
+++ Backend/routes/blogScraper.js
@@ -4,27 +4,27 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const router = express.Router();
 
-router.get('/scrape-techcrunch', async (req, res) => {
-  try {
-    const response = await axios.get('https://techcrunch.com/');
-    const $ = cheerio.load(response.data);
-    const articles = [];
+app.get('/api/blogs/scrape-techcrunch', async (req, res) => {
+    try {
+        const { data } = await axios.get('https://techcrunch.com/');
+        const $ = cheerio.load(data);
+        const articles = [];
 
-    $('article.post-block').each((i, el) => {
-      const title = $(el).find('h2.post-block__title a').text().trim();
-      const url = $(el).find('h2.post-block__title a').attr('href');
-      const image = $(el).find('img').attr('src');
+        $('.post-block').each((i, el) => {
+            const title = $(el).find('.post-block__title a').text().trim();
+            const url = $(el).find('.post-block__title a').attr('href');
+            const image = $(el).find('img').attr('src');
 
-      if (title && url && image) {
-        articles.push({ title, url, image });
-      }
-    });
+            if (title && url && image) {
+                articles.push({ title, url, image });
+            }
+        });
 
-    res.json({ articles });
-  } catch (err) {
-    console.error('Scrape failed:', err.message);
-    res.status(500).json({ error: 'Scraping failed' });
-  }
+        res.json({ articles });
+    } catch (err) {
+        console.error('Scraping failed:', err.message);
+        res.status(500).json({ error: 'Scraping failed' });
+    }
 });
 
 module.exports = router;
